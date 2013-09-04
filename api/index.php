5afd3af4b7b1a1cac56eb28af54a05e5c47e5b92
Tran Luong Quoc Dai <daitlq@gmail.com>
2013-09-04 10:13:20 +0700

    + Add api to get all books.

---
+++ api/index.php
@@ -0,0 +1,35 @@
+<?php
+
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+$app->get('/books', 'getAllBooks');
+
+$app->run();
+
+function getAllBooks() {
+	$sql = "select * FROM book";
+	try {
+		$db = getConnection();
+		$stmt = $db->query($sql);  
+		$wines = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$db = null;
+		echo json_encode($wines);
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+	}
+}
+
+function getConnection() {
+	$dbhost="127.0.0.1";
+	$dbuser="root";
+	$dbpass="";
+	$dbname="lucbinh_collection";
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $dbh;
+}
+
+?>
